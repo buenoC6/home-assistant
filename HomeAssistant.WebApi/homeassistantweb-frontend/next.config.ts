e50b6e151fd2954e7404778afd27ff96fb5c5228
Alexian Moins <alexian.moins@outlook.com>
2024-12-02 20:30:50 +0100

    graph

--- HomeAssistant.WebApi/homeassistantweb-frontend/next.config.ts
+++ HomeAssistant.WebApi/homeassistantweb-frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        domains: ["uni003eu5.fusionsolar.huawei.com"], // Ajoutez ici le domaine de l'image
+    },
 };
 
 export default nextConfig;

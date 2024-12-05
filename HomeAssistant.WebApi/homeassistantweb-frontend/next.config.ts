524e5e93d818dc906a881569f642875493e39554
Alexian Moins <alexian.moins@outlook.com>
2024-12-05 19:42:06 +0100

    CORS

--- HomeAssistant.WebApi/homeassistantweb-frontend/next.config.ts
+++ HomeAssistant.WebApi/homeassistantweb-frontend/next.config.ts
@@ -4,21 +4,20 @@ const nextConfig: NextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)', // Match toutes les routes
+        source: '/(.*)',
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: `
-              default-src 'self' data: https://uni003eu5.fusionsolar.huawei.com;
-              style-src 'self' 'unsafe-inline' fonts.googleapis.com;
-              base-uri 'none';
-              form-action 'self';
-              script-src 'self' 'unsafe-eval' 'unsafe-inline';
-              font-src 'self' fonts.gstatic.com;
-              manifest-src 'self';
-              img-src 'self' blob: https://uni003eu5.fusionsolar.huawei.com data:;
-              connect-src 'self' 'unsafe-inline' 'unsafe-eval' data: http://192.168.1.37:8080 http://192.168.1.37:8080/api/network/onduleur;
-            `,
+            value:
+              "default-src 'self' data: https://uni003eu5.fusionsolar.huawei.com; " +
+              "style-src 'self' 'unsafe-inline' fonts.googleapis.com; " +
+              "base-uri 'none'; " +
+              "form-action 'self'; " +
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline'; " +
+              "font-src 'self' fonts.gstatic.com; " +
+              "manifest-src 'self'; " +
+              "img-src 'self' blob: https://uni003eu5.fusionsolar.huawei.com data:; " +
+              "connect-src 'self' 'unsafe-inline' 'unsafe-eval' data: http://192.168.1.37:8080 http://192.168.1.37:8080/api/network/onduleur;",
           },
         ],
       },
